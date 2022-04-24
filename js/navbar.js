ed4382dd895359c7adcc5ee90e76ab3454265202
Emmanuel Adolfo Méndez Retana <Emmanuelmendezretana@gmail.com>
2022-04-24 13:33:44 -0600

    Add event in navbar links

--- js/navbar.js
+++ js/navbar.js
@@ -1,1 +1,13 @@
+$ = id => document.getElementById(id);
 $$ = selectors => document.querySelectorAll(selectors);
+
+const updateActiveClass = id => {
+    $(id).addEventListener('click', event => {
+        if (event.target.classList.contains('nav-link')) {
+            $$(`#${id} .nav-item .nav-link`).forEach(link => link.classList.remove('active'));
+            event.target.classList.add('active');
+        }
+    });
+}
+
+updateActiveClass('menuNavbar');
