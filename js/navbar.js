4f61cffb87f7bb4020a790f48070904d4ced3bc4
Emmanuel Adolfo Méndez Retana <Emmanuelmendezretana@gmail.com>
2022-04-29 11:40:15 -0600

    Add portfolio and intersection observer logic

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,29 @@
 $ = id => document.getElementById(id);
 $$ = selectors => document.querySelectorAll(selectors);
 
+const observeActiveClass = element => {
+	const callback = (entries) => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				$$(`#menuNavbar .nav-item .nav-link`).forEach(link => {
+					if (link.getAttribute('href') == `#${element}`) {
+						link.classList.add('active');
+					} else {
+						link.classList.remove('active');
+					}
+				});
+			}
+		});
+	};
+	const options = {
+		root: null,
+		rootMargin: '0px',
+		threshold: 0.2,
+	};
+
+	return new IntersectionObserver(callback, options).observe($(element));
+};
+
 const updateActiveClass = id => {
 	$(id).addEventListener('click', event => {
 		if (event.target.classList.contains('nav-link')) {
@@ -12,4 +35,8 @@ const updateActiveClass = id => {
 	});
 };
 
-updateActiveClass('menuNavbar');
+observeActiveClass('home');
+observeActiveClass('services');
+observeActiveClass('portfolio');
+
+updateActiveClass('portfolioNavbar');
